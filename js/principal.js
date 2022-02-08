053d1d81d8c2aa3e9a75f2f0b38b3808140430e9
Felix <felixklkl188@gmail.com>
2022-02-08 15:47:24 -0300

    Iniciando arquivo index.html de calculo de imc

---
+++ js/principal.js
@@ -0,0 +1,60 @@
+var titulo = document.querySelector(".titulo");
+
+titulo.textContent = "Índice de Massa Corporal (IMC)";
+
+var pacientes = document.querySelectorAll(".paciente");
+
+for (var i = 0; i < pacientes.length; i++) {
+	
+	var paciente = pacientes[i];
+
+	var tdPeso = paciente.querySelector(".info-peso");
+	var peso = tdPeso.textContent;
+
+	var tdAltura = paciente.querySelector(".info-altura");
+	var altura = tdAltura.textContent;
+
+	var tdImc = paciente.querySelector(".info-imc");
+
+	var pesoValido = validaPeso(peso);
+	var alturaValida = validaAltura(altura);
+
+	if (!pesoValido) {
+		pesoValido = false;
+		tdImc.textContent = "Peso inválido!";
+		paciente.classList.add("paciente-invalido");
+	}
+
+	if (!alturaValida) {
+		alturaValida = false;
+		tdImc.textContent = "Altura inválida!"
+		paciente.classList.add("paciente-invalido");
+	}
+
+	if (alturaValida && pesoValido) {
+		var imc = calculaImc(peso, altura);
+		tdImc.textContent = imc;
+	}
+}
+
+function calculaImc(peso, altura){
+	imc = 0;
+	imc = peso / (altura * altura);
+	return imc.toFixed(2);
+}
+
+function validaPeso(peso) {
+	if(peso >= 0 && peso < 500){
+		return true;
+	}else{
+		return false;
+	}  
+}
+
+function validaAltura(altura) {
+	if(altura >= 0 && altura < 3.0){
+		return true;
+	}else{
+		return false;
+	}
+}
